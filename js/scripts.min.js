855aae23fb306ac30212c0a6085a2a0966552da2
timshoe <tim.xu67@gmail.com>
2019-09-03 20:44:53 -0400

    Resume updates, information updates

--- js/scripts.min.js
+++ js/scripts.min.js
@@ -1,8 +1,4 @@
 /*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
     Repo: https://github.com/RyanFitzgerald/devportfolio-template
     Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
 
